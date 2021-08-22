514c58f629b7dd34bc18b88476eaf9b9ab476189
Sungwon Han <holyhansss@gmail.com>
2021-08-22 23:53:41 +0900

    working on create Dontation Item function Test

--- test/NFTMarket.test.js
+++ test/NFTMarket.test.js
@@ -1,25 +1,30 @@
 const Market = artifacts.require('NFTMarket');
 const NFT = artifacts.require('NFT');
 
-contract('NFT Market', () => {
-    it('Should be add org', async () => {
+contract('NFT Market', async (accounts) => {
+    it('Should be add organization correctly', async () => {
+        const orgAddress = accounts[0];
         const market = await Market.deployed();
-        await market.addOrganization("0x0d3D56806fe6eeDe410Ea9722da9a6f65FD24799", "Malie")
+        await market.addOrganization(orgAddress, "Milal")
         const b = await market.getOrgAddressById(0)
-        assert.equal(b, "0x0d3D56806fe6eeDe410Ea9722da9a6f65FD24799");
+        assert.equal(orgAddress, b);
     });
     
-    it('create Donation Item', async () => {
+    it.only('create Donation Item', async () => {
+        
         const market = await Market.deployed();
         const nft = await NFT.deployed();
-        await market.addOrganization("0x0d3D56806fe6eeDe410Ea9722da9a6f65FD24799", "Malie")
-        let price = await market.getListingPrice({from: "0x0d3D56806fe6eeDe410Ea9722da9a6f65FD24799"});
-        console.log(web3.utils.toWei(price, "ether"));
-        //await NFT.createToken('URL');
-        console.log(nft.address)
-        //await market.createMarketItem(NFT.address, 0, 1, "0x0d3D56806fe6eeDe410Ea9722da9a6f65FD24799", {from: "0x0d3D56806fe6eeDe410Ea9722da9a6f65FD24789", value: price} )
-        //const b = await market.getOrgAddressById(0)
-        assert.equal(1,1);
+
+        const orgAddress = accounts[0];
+        const minter = accounts[1];
+
+        await market.addOrganization(orgAddress, "Milal")
+        let listingPrice = await market.getListingPrice();
+        listingPrice = listingPrice.toString();
+        listingPrice = web3.utils.fromWei(listingPrice, "ether")
+                
+
+        assert.equal(0.005, listingPrice);
     });
 
 })
