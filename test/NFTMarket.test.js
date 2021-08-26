4ceb85be77206a7062a3dede999c1ebe07c79ef5
Sungwon Han <holyhansss@gmail.com>
2021-08-27 00:05:13 +0900

    check token has transfered correctly

--- test/NFTMarket.test.js
+++ test/NFTMarket.test.js
@@ -40,16 +40,24 @@ contract('NFT Market', async (accounts) => {
         const nft = await NFT.deployed();
 
         const owner = accounts[0]; // deployer 
-        const orgAddress = accounts[1];
-        const minter = accounts[2];
-        const buyer = accounts[3];
+        const orgAddress = accounts[5];
+        const minter = accounts[6];
+        const buyer = accounts[7];
+
+        console.log("owner: " + owner)
+        console.log("orgAddress: " + orgAddress)
+        console.log("minter: " + minter)
+        console.log("buyer: " + buyer)
+        console.log("nft Address: " + nft.address)
+        console.log("nftMarket Address: " + market.address)
 
         let listingPrice = await market.getListingPrice();
         listingPrice = listingPrice.toNumber();
         //create token and market item
-        await nft.createToken("URL", {from: minter})
+        await nft.createToken("SOME URL", {from: minter})
         await market.createMarketItem(nft.address, 0, "5000000000000000000", orgAddress, {from: minter, value: listingPrice})
         let minterPriceBeforeCreateSale = await web3.eth.getBalance(minter);
+
         // create market sale
         await market.createMarketSale(nft.address, 0, {from: buyer, value: "5000000000000000000"});
 
@@ -59,8 +67,9 @@ contract('NFT Market', async (accounts) => {
         assert.equal(Number(minterPriceBeforeCreateSale) + 500000000000000000, minterPriceInWei) // earn 10% of item price
         assert.equal(orgPriceInWei, 104500000000000000000) // get 100 ethers when deployed, and 90% of price 
         
-        //check owner
-        //assert.equal()
+        //check token has transfered correctly
+        tokenOwner = await nft.ownerOf(0, {from: buyer});
+        assert.equal(tokenOwner, buyer)
     });
 
 })
