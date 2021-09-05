7ccbf07bec8748a482e95d5c30bae5b8de383711
Sungwon Han <holyhansss@gmail.com>
2021-09-06 00:00:59 +0900

    Create New Org

--- client/src/component/CreateNewOrg.js
+++ client/src/component/CreateNewOrg.js
@@ -2,26 +2,27 @@ import React, { Component } from "react";
 
 class CreateNewOrg extends Component {
     
-  constructor(props){
-    super(props)
-    
-    this.state = {
-        web3: this.props.web3,
-        NFTContract: this.props.NFTContract, 
-        NFTContractAddress: this.props.NFTContractAddress,
-        NFTMarketContract: this.props.NFTMarketContract, 
-        account: this.props.account,
-        selectedFile: null,
-        url: '',
-        assetName:null,
-        assetDesc:null,
-        assetPrice:null,
-        sellCheckbox: false,
-    };
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            web3: this.props.web3,
+            NFTContract: this.props.NFTContract, 
+            NFTContractAddress: this.props.NFTContractAddress,
+            NFTMarketContract: this.props.NFTMarketContract, 
+            account: this.props.account,       
+            OrganizationName: '', 
+            OrganizationAddress: '',
+        };
 
-  }
+    }
+
+    inputHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
     CreateOrg = async () => {
-
+        
     }
 
     render() {
