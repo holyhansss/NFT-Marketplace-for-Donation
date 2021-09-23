921b22a5d8aa989d74287aae1749596a32b6cdb7
Sungwon Han <holyhansss@gmail.com>
2021-09-23 22:40:21 +0900

    menu bar style

--- client/src/component/Controller.js
+++ client/src/component/Controller.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-
+import "./style/Controller.css"
 class Controller extends Component {
 
   render() {
       
     return (
-      <ul>
-        <li onClick={function(e){
-            this.props.onChangeMode('create');
-        }.bind(this)}>Create</li>
-        <li onClick={function(e){
-            this.props.onChangeMode('viewNFTMarket');
-        }.bind(this)}>NFT Market</li>
-        <li onClick={function(e){
-            this.props.onChangeMode('viewMyItem');
-        }.bind(this)}>View My Item</li>
-        <li onClick={function(e){
-            this.props.onChangeMode('createNewOrg');
-        }.bind(this)}>Create New Organization</li>
-        
-      </ul>
+      <div className="menuBar">
+        <ul className="tabs">
+          <li onClick={function(e){
+              this.props.onChangeMode('create');
+          }.bind(this)}>Create</li>
+          <li onClick={function(e){
+              this.props.onChangeMode('viewNFTMarket');
+          }.bind(this)}>NFT Market</li>
+          <li onClick={function(e){
+              this.props.onChangeMode('viewMyItem');
+          }.bind(this)}>View My Item</li>
+          <li onClick={function(e){
+              this.props.onChangeMode('createNewOrg');
+          }.bind(this)}>Create New Organization</li>
+        </ul>
+      </div>
     );
   }
 }
